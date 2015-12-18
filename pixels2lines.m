8ad96280b5b2ce4ba2acae5cdec9d5134a7a02c6
bald6354 <bald6354@yahoo.com>
2015-12-18 15:43:00 -0500

    Update pixels2lines.m
    Bug fixes and now works on image chips with offsets and not the entire datacube all the time (much faster)

--- pixels2lines.m
+++ pixels2lines.m
@@ -1,7 +1,5 @@
-function lines = pixels2lines(skelIm)
-
+function lines = pixels2lines(skelIm, debug)
 %Takes an skeletonized image as an input and extracts lines
-lines = cell(0,1);
 
 if false
     %From Mathworks
@@ -10,58 +8,77 @@ if false
     skelIm(128,:) = 0;
 end
 
+%Default debug to false
+if ~exist('debug','var')
+    debug = '0';
+end
+
+%Convert string to number
+debug = str2num(debug);
+
+%Init
+lines = cell(0,1);
+
 %Group all connected pixels
 CC = bwconncomp(skelIm);
+S = regionprops(CC,'BoundingBox','Image');
 
-for grpid = 1:numel(CC.PixelIdxList)
+for grpid = 1:numel(S)
 
-    %Make an image from the group
-    grpim = zeros(size(skelIm));
-    grpim(CC.PixelIdxList{grpid}) = 1;
+    if debug
+        clc, grpid
+    end
     
+    %Get an image from the group
+    grpim = S(grpid).Image;
+
+    %Get the offsets to the full image
+    offsets = floor(S(grpid).BoundingBox(2:-1:1));
+
     %while we still have pixels to extract
     while(sum(grpim(:)))
 
+        if debug
+            sum(grpim(:))
+        end
+    
         %Find remanining critical points
-%         [ep(:,1), ep(:,2)] = find(bwmorph(grpim,'endpoints'));
-%         [bp(:,1), bp(:,2)] = find(bwmorph(grpim,'branchpoints'));
         [ep, bp] = vipPixelFinder(grpim);
         targets = cat(1, ep, bp);
         
-        imagesc(grpim)
-        hold on
-        plot(bp(:,2),bp(:,1),'rx')
-        plot(ep(:,2),ep(:,1),'go')
+        if debug
+            clf
+            imagesc(grpim)
+            hold on
+            plot(bp(:,2),bp(:,1),'rx')
+            plot(ep(:,2),ep(:,1),'go')
+        end
 
         %Check if the image is a single circle
         if isempty(targets)
             [targets(1,1) targets(1,2)] = find(grpim,1);
         end
+        
         %Trace from the first endpoint to another endpoint or branchpoint
         line = pixelWalker(grpim, targets(1,:), targets(2:end,:));
         
-        plot(line(:,2),line(:,1),'k')
-        
-%         pause(2)
+        if debug
+            plot(line(:,2),line(:,1),'k')
+            pause(1)
+        end
         
         %Remove the selected endpoint to the critical point
         grpim(sub2ind(size(grpim),line(1:end-1,1),line(1:end-1,2))) = 0;
         
-        %If the last point in the line was an endpoint it is ok to remove that as well
-        if (ismember(line(end,:),ep,'rows') | (sum(grpim(:))==1))
+        %Remove the last point unless it was a branch point
+        if ~(ismember(line(end,:),bp,'rows'))
              grpim(line(end,1),line(end,2)) = 0;
         end
         
         %Save the line
-        lines(end+1) = {line};
+        lines(end+1) = {line+repmat(offsets,size(line,1),1)};
         
         clear bp ep
-        
-%         imagesc(grpim)
-%         plot(line(:,2),line(:,1),'r')
-%         xlim([80 205])
-%         ylim([120 240])
-        
+
     end  
-    disp('done')
 end
